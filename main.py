e29ad590d166b8651b1bbeca6b7ba25407eb94e2
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-10-04 18:04:03 +0300

    Проба пера

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-# This is a sample Python script.
+import datetime as dt
+import ipinfo
+import json
+import requests
+import yaml
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def get_initial_parameters() -> dict:
+    with open("properties.yaml", "r") as yaml_file:
+        try:
+            data = yaml.safe_load(yaml_file)
+
+            return data
+        except yaml.YAMLError as exc:
+            print("Ошибка при чтении YAML-файла:", exc)
+
+def get_city_by_ip(ip: str = '') -> str:
+    handler = ipinfo.getHandler(IP_INFO_API_KEY)
+    details = handler.getDetails(ip)
+
+    return details.all.get('city')
+
+def get_celsius_from_response(response: json) -> float:
+    kelvin = response.get("main").get("temp")
+    return kelvin - 273.15
 
 
+if __name__ == "__main__":
+    data = get_initial_parameters()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    BASE_URL = data.get('openweathermap').get('base_url')
+    IP_INFO_API_KEY = data.get('ipinfo').get('api_key')
+    API_KEY_OPEN_WEATHER = data.get('openweathermap').get('api_key')
+
+    CITY = get_city_by_ip()
+
+    url = BASE_URL + '?' + "appid=" + API_KEY_OPEN_WEATHER + "&q=" + CITY
+    response = requests.get(url).json()
+
+    print(response)
+    print(get_celsius_from_response(response))
